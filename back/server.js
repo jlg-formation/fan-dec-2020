587ddfaea024876ecedb5bb9bb8ad763189ef103
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-21 12:11:42 +0100

    created public folder

--- back/server.js
+++ back/server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const serveIndex = require("serve-index");
 const app = express();
 const port = 3000;
+const www = "public";
 
 app.use(function (req, res, next) {
   console.log("req.url", req.url);
   next();
 });
 
-app.use(express.static("."));
-app.use(serveIndex("."));
+app.use(express.static(www));
+app.use(serveIndex(www));
 
 app.listen(port, () => {
   console.log("Server started with success on port " + port);
