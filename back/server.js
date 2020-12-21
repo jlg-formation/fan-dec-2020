ed6cd85f63e0ddcc67107254350503b52f231413
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-21 14:35:12 +0100

    added angularjs

--- back/server.js
+++ back/server.js
@@ -3,12 +3,14 @@ const serveIndex = require("serve-index");
 const app = express();
 const port = 3000;
 const www = "public";
+const node_modules = "node_modules";
 
 app.use(function (req, res, next) {
   console.log("req.url", req.url);
   next();
 });
 
+app.use(express.static(node_modules));
 app.use(express.static(www));
 app.use(serveIndex(www));
 
