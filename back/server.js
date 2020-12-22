0e60719707f1e93f5f198c2fa5274aca89a0ef40
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-22 10:09:05 +0100

    added url rewriting

--- back/server.js
+++ back/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const serveIndex = require("serve-index");
+const path = require("path");
 const app = express();
 const port = 3000;
 const www = "public";
@@ -13,6 +14,10 @@ app.use(function (req, res, next) {
 app.use(express.static(node_modules));
 app.use(express.static(www));
 app.use(serveIndex(www));
+
+app.use(function (req, res, next) {
+  res.sendFile(path.resolve(__dirname, "./public/index.html"));
+});
 
 app.listen(port, () => {
   console.log("Server started with success on port " + port);
