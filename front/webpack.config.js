3cd22d9b04b8486f6021edb38436d69dca3335cf
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-23 14:05:50 +0100

    add clean dist webpack

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -18,6 +19,7 @@ module.exports = {
       patterns: [{ from: "src/assets", to: "assets" }],
     }),
     new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
   ],
   module: {
     rules: [
