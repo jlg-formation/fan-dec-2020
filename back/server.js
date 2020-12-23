09650aaa7dab3c5bb33a5e1699f9672648b4647b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-23 15:57:33 +0100

    ready to pm2

--- back/server.js
+++ back/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const serveIndex = require("serve-index");
 const path = require("path");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const www = "../front/dist";
 const node_modules = "node_modules";
 
